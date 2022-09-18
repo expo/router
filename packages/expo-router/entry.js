585f696c65c5e5ebfd5de62b40181ebf11d2b364
evanbacon <bacon@expo.io>
2022-09-17 22:57:07 -0500

    add top-level error handling

--- packages/expo-router/entry.js
+++ packages/expo-router/entry.js
@@ -2,6 +2,7 @@ import "@bacons/expo-metro-runtime";
 
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
+import { View, Platform } from "react-native";
 
 // Must be exported or Fast Refresh won't update the context >:[
 export function App() {
@@ -11,4 +12,72 @@ export function App() {
   return <ExpoRoot context={ctx} />;
 }
 
-registerRootComponent(App);
+function isBaseObject(obj) {
+  if (Object.prototype.toString.call(obj) !== "[object Object]") {
+    return false;
+  }
+  const proto = Object.getPrototypeOf(obj);
+  if (proto === null) {
+    return true;
+  }
+  return proto === Object.prototype;
+}
+
+function isErrorShaped(error) {
+  return (
+    error &&
+    typeof error === "object" &&
+    typeof error.name === "string" &&
+    typeof error.message === "string"
+  );
+}
+
+/**
+ * After we throw this error, any number of tools could handle it.
+ * This check ensures the error is always in a reason state before surfacing it to the runtime.
+ */
+function convertError(error) {
+  if (isErrorShaped(error)) {
+    return error;
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    if (error == null) {
+      return new Error("A null/undefined error was thrown.");
+    }
+  }
+
+  if (isBaseObject(error)) {
+    return new Error(JSON.stringify(error));
+  }
+
+  return new Error(String(error));
+}
+
+(() => {
+  try {
+    registerRootComponent(App);
+  } catch (e) {
+    const error = convertError(e);
+    // Prevent the app from throwing confusing:
+    //  ERROR  Invariant Violation: "main" has not been registered. This can happen if:
+    // * Metro (the local dev server) is run from the wrong folder. Check if Metro is running, stop it and restart it in the current project.
+    // * A module failed to load due to an error and `AppRegistry.registerComponent` wasn't called.
+    registerRootComponent(View);
+
+    // Console is pretty useless on native, on web you get interactive stack traces.
+    if (Platform.OS === "web") {
+      console.error(error);
+      console.error(
+        `A runtime error has occurred while rendering the root component.`
+      );
+    }
+
+    // Give React a tick to render before throwing.
+    setTimeout(() => {
+      throw error;
+    });
+
+    // TODO: Render a production-only error screen.
+  }
+})();
