daabd1eb2489bea09ac47f6fe065897afd121ee9
evanbacon <bacon@expo.io>
2022-09-19 20:40:28 -0500

    fix dynamic redirect

--- packages/expo-router/src/matchers.tsx
+++ packages/expo-router/src/matchers.tsx
@@ -1,15 +1,15 @@
 /** Match `[page]` -> `page` */
 export function matchDynamicName(name: string): string | undefined {
     // Don't match `...` or `[` or `]` inside the brackets
-    return name.match(/^\[([^/\[\](?:\.\.\.)]+)\]$/)?.[1];
+    return name.match(/^\[([^/\[\](?:\.\.\.)]+?)\]$/)?.[1];
 }
 
 /** Match `[...page]` -> `page` */
 export function matchDeepDynamicRouteName(name: string): string | undefined {
-    return name.match(/^\[\.\.\.([^/]+)\]$/)?.[1];
+    return name.match(/^\[\.\.\.([^/]+?)\]$/)?.[1];
 }
 
 /** Match `(page)` -> `page` */
 export function matchFragmentName(name: string): string | undefined {
-    return name.match(/^\(([^/]+)\)$/)?.[1];
+    return name.match(/^\(([^/]+?)\)$/)?.[1];
 }
