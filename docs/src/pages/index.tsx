0aec84016033d60069173f973a90813bb15e28ba
Cedric van Putten <github@cedric.dev>
2022-09-27 14:13:48 -0700

    docs: add metatag description for the docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="File System-based Routing for React Native apps">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
