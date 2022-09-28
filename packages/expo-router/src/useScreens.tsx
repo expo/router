9c653ad254e6c720e09301663dd2e67740806081
Evan Bacon <bacon@expo.io>
2022-09-28 14:45:58 -0500

    evaluate components lazily using getComponent (#18)

--- packages/expo-router/src/useScreens.tsx
+++ packages/expo-router/src/useScreens.tsx
@@ -1,68 +1,85 @@
-import React from 'react';
+import React from "react";
 
-import { Screen } from './primitives';
-import { Route, RouteNode, sortRoutes, useRoutes } from './Route';
-import { Try } from './views/Try';
+import { Screen } from "./primitives";
+import { Route, RouteNode, sortRoutes, useRoutes } from "./Route";
+import { Try } from "./views/Try";
 
 /**
  * @returns React Navigation screens for the route.
  */
 export function useScreens(): React.ReactNode[] {
-    const children = useRoutes();
-    return React.useMemo(() => children.map((value) => routeToScreen(value)), [children]);
+  const children = useRoutes();
+  return React.useMemo(
+    () => children.map((value) => routeToScreen(value)),
+    [children]
+  );
 }
 
-export type ScreenProps<TOptions extends Record<string, any> = Record<string, any>> = {
-    /** Name is required when used inside a Layout component. */
-    name?: string,
-    initialParams?: { [key: string]: any };
-    options?: TOptions;
+export type ScreenProps<
+  TOptions extends Record<string, any> = Record<string, any>
+> = {
+  /** Name is required when used inside a Layout component. */
+  name?: string;
+  initialParams?: { [key: string]: any };
+  options?: TOptions;
+};
+
+function getSortedChildren(
+  children: RouteNode[],
+  order?: ScreenProps[]
+): { route: RouteNode; props: any }[] {
+  if (!order?.length) {
+    return children.sort(sortRoutes).map((route) => ({ route, props: {} }));
+  }
+  const entries = [...children];
+
+  const ordered = order
+    .map(({ name, initialParams, options }) => {
+      if (!entries.length) {
+        console.warn(
+          `[Layout children]: Too many screens defined. Route "${name}" is extraneous.`
+        );
+        return null;
+      }
+      const matchIndex = entries.findIndex((child) => child.route === name);
+      if (matchIndex === -1) {
+        console.warn(
+          `[Layout children]: No route named "${name}" exists in nested children:`,
+          children.map(({ route }) => route)
+        );
+        return null;
+      } else {
+        // Get match and remove from entries
+        const match = entries[matchIndex];
+        entries.splice(matchIndex, 1);
+
+        return { route: match, props: { initialParams, options } };
+      }
+    })
+    .filter(Boolean) as {
+    route: RouteNode;
+    props: Partial<ScreenProps>;
+  }[];
+
+  // Add any remaining children
+  ordered.push(
+    ...entries.sort(sortRoutes).map((route) => ({ route, props: {} }))
+  );
+
+  return ordered;
 }
 
-function getSortedChildren(children: RouteNode[], order?: ScreenProps[]): { route: RouteNode, props: any }[] {
-    if (!order?.length) {
-        return children.sort(sortRoutes).map((route) => ({ route, props: {} }));
-    }
-    const entries = [...children];
-
-    const ordered = order.map(({ name, initialParams, options }) => {
-        if (!entries.length) {
-            console.warn(`[Layout children]: Too many screens defined. Route "${name}" is extraneous.`);
-            return null;
-        }
-        const matchIndex = entries.findIndex((child) => child.route === name)
-        if (matchIndex === -1) {
-            console.warn(`[Layout children]: No route named "${name}" exists in nested children:`, children.map(({ route }) => route));
-            return null;
-        } else {
-            // Get match and remove from entries
-            const match = entries[matchIndex];
-            entries.splice(matchIndex, 1);
-
-            return { route: match, props: { initialParams, options } }
-        }
-    }).filter(Boolean) as {
-        route: RouteNode;
-        props: Partial<ScreenProps>;
-    }[]
-
-    // Add any remaining children
-    ordered.push(...entries.sort(sortRoutes).map((route) => ({ route, props: {} })));
-
-    return ordered;
-}
-
-
-/** 
+/**
  * @returns React Navigation screens sorted by the `route` property.
  */
 export function useSortedScreens(order: ScreenProps[]): React.ReactNode[] {
-    const children = useRoutes();
-    const sorted = getSortedChildren(children, order);
-    return React.useMemo(() =>
-        sorted.map((value) => routeToScreen(value.route, value.props)), [sorted]);
+  const children = useRoutes();
+  const sorted = getSortedChildren(children, order);
+  return React.useMemo(
+    () => sorted.map((value) => routeToScreen(value.route, value.props)),
+    [sorted]
+  );
 }
-
 
 // TODO: Maybe there's a more React-y way to do this?
 // Without this store, the process enters a recursive loop.
@@ -70,58 +87,66 @@ const qualifiedStore = new WeakMap<RouteNode, React.ComponentType<any>>();
 
 /** Wrap the component with various enhancements and add access to child routes. */
 function getQualifiedRouteComponent(value: RouteNode) {
-    if (qualifiedStore.has(value)) {
-        return qualifiedStore.get(value);
+  if (qualifiedStore.has(value)) {
+    return qualifiedStore.get(value)!;
+  }
+
+  const Component = value.getComponent();
+
+  const { ErrorBoundary } = value.getExtras();
+
+  const QualifiedRoute = React.forwardRef(
+    (props: { route: any; navigation: any }, ref: any) => {
+      // Surface dynamic name as props to the view
+      const children = React.createElement(Component, {
+        ...props,
+        ref,
+      });
+
+      const errorBoundary = ErrorBoundary ? (
+        <Try catch={ErrorBoundary}>{children}</Try>
+      ) : (
+        children
+      );
+
+      return <Route filename={value.contextKey}>{errorBoundary}</Route>;
     }
+  );
 
-    const Component = value.getComponent();
-
-    const { ErrorBoundary } = value.getExtras();
-
-    const QualifiedRoute = React.forwardRef(
-        (props: { route: any; navigation: any }, ref: any) => {
-            // Surface dynamic name as props to the view
-            const children = React.createElement(Component, {
-                ...props,
-                ref,
-            });
-
-            const errorBoundary = ErrorBoundary ? (
-                <Try catch={ErrorBoundary}>{children}</Try>
-            ) : (
-                children
-            );
-
-            return (
-                <Route filename={value.contextKey}>
-                    {errorBoundary}
-                </Route>
-            );
-        }
-    );
-
-    QualifiedRoute.displayName = `Route(${Component.displayName || Component.name || value.route})`;
-    qualifiedStore.set(value, QualifiedRoute);
-    return QualifiedRoute;
+  QualifiedRoute.displayName = `Route(${
+    Component.displayName || Component.name || value.route
+  })`;
+  qualifiedStore.set(value, QualifiedRoute);
+  return QualifiedRoute;
 }
 
-function routeToScreen(route: RouteNode, { options, ...props }: Partial<ScreenProps> = {}) {
-    const staticOptions = route.getExtras()?.getNavOptions;
-    return (
-        // @ts-expect-error: fixme
-        <Screen
-            {...props}
-            name={route.route}
-            key={route.route}
-            options={options ? (args) => {
-                const staticResult = typeof staticOptions === 'function' ? staticOptions(args) : staticOptions;
-                const dynamicResult = typeof options === 'function' ? options?.(args) : options;
-                return {
-                    ...staticResult,
-                    ...dynamicResult,
-                };
-            } : staticOptions}
-            component={getQualifiedRouteComponent(route)}
-        />
-    );
+function routeToScreen(
+  route: RouteNode,
+  { options, ...props }: Partial<ScreenProps> = {}
+) {
+  const staticOptions = route.getExtras()?.getNavOptions;
+  return (
+    <Screen
+      {...props}
+      name={route.route}
+      key={route.route}
+      options={
+        options
+          ? (args) => {
+              const staticResult =
+                typeof staticOptions === "function"
+                  ? staticOptions(args)
+                  : staticOptions;
+              const dynamicResult =
+                typeof options === "function" ? options?.(args) : options;
+              return {
+                ...staticResult,
+                ...dynamicResult,
+              };
+            }
+          : staticOptions
+      }
+      getComponent={() => getQualifiedRouteComponent(route)}
+    />
+  );
 }
