17613b49aabd2b675b8676e5facb4971cacedcc2
trin <trin4ik@gmail.com>
2022-09-29 10:56:08 +0300

    fix demo app

--- apps/demo/app/(root)/modal.tsx
+++ apps/demo/app/(root)/modal.tsx
@@ -1,36 +1,39 @@
-import { Link, useLink } from "expo-router";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {Link, useLink} from "expo-router";
+import {StatusBar, StyleSheet, Text, View} from "react-native";
 
-export default function App({ navigation }) {
-  const link = useLink();
+export default function App({navigation}) {
+    const link = useLink();
 
-  return (
-    <View style={styles.container}>
-      <Link href="../" style={{ fontSize: 24, fontWeight: "bold" }}>
-        Dismiss
-      </Link>
-      <Text
-        onPress={() => {
-          link.back();
-        }}
-      >
-        Open settings dynamic
-      </Text>
-      <StatusBar barStyle="light-content" />
-      {navigation.canGoBack() ? (
-        <span>Can go back</span>
-      ) : (
-        <span>Can't go back</span>
-      )}
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Link to="../" style={{fontSize: 24, fontWeight: "bold"}}>
+                Dismiss
+            </Link>
+            <Text
+                onPress={() => {
+                    link.push({
+                        pathname: "/settings/[home]",
+                        query: {home: "baconbrix"},
+                    })
+                }}
+            >
+                Open settings dynamic
+            </Text>
+            <StatusBar barStyle="light-content"/>
+            {navigation.canGoBack() ? (
+                <Text>Can go back</Text>
+            ) : (
+                <Text>Can't go back</Text>
+            )}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#fff",
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#fff",
+        alignItems: "center",
+    },
 });
--- apps/demo/metro.config.js
+++ apps/demo/metro.config.js
@@ -1,22 +1,23 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo-router/metro-config");
-const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config")
+const path = require("path")
 
 // Find the project and workspace directories
-const projectRoot = __dirname;
+const projectRoot = __dirname
 // This can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(projectRoot, "../..");
+const workspaceRoot = path.resolve(projectRoot, "../..")
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+config.watchFolders = [workspaceRoot]
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
+	path.resolve(projectRoot, "node_modules"),
+	path.resolve(workspaceRoot, "node_modules"),
+]
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+config.resolver.disableHierarchicalLookup = true
+config.transformer.unstable_allowRequireContext = true
 
-module.exports = config;
+module.exports = config
