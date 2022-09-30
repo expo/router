886572511ef943fc115d3897c23242302159060f
Evan Bacon <bacon@expo.io>
2022-09-30 15:33:32 -0500

    feat: add statePath and normalized pathname to custom layouts (#32)
    * feat: add statePath and normalized pathname to custom layouts
    
    * Update Layout.tsx

--- packages/expo-router/src/views/Layout.tsx
+++ packages/expo-router/src/views/Layout.tsx
@@ -1,4 +1,5 @@
 import {
+  LinkingContext,
   RouterFactory,
   StackRouter,
   useNavigationBuilder,
@@ -6,12 +7,18 @@ import {
 import * as React from "react";
 
 import { useContextKey } from "../Route";
+import getPathFromState from "../fork/getPathFromState";
+import { resolveHref } from "../link/href";
+import { matchFragmentName } from "../matchers";
 import { useScreens } from "../useScreens";
 
 // TODO: This might already exist upstream, maybe something like `useCurrentRender` ?
 export const LayoutContext = React.createContext<{
   contextKey: string;
+  /** Normalized path representing the selected route `/[id]?id=normal` -> `/normal` */
   pathname: string;
+  /** Normalized string representing the selected state `/(group)/any/[id]` */
+  statePath: string;
   state: any;
   navigation: any;
   descriptors: any;
@@ -40,6 +47,7 @@ export function Layout({
 }: LayoutProps) {
   const contextKey = useContextKey();
   const screens = useScreens();
+  const linking = React.useContext(LinkingContext);
 
   const { state, navigation, descriptors, NavigationContent } =
     useNavigationBuilder(router, {
@@ -48,12 +56,15 @@ export function Layout({
       initialRouteName,
     });
 
-  const selected = state?.routes[state.index]?.name;
+  const statePath = linking.options?.getPathFromState
+    ? linking.options.getPathFromState(state)
+    : getPathFromState(state);
 
   return (
     <LayoutContext.Provider
       value={{
-        pathname: selected ?? "",
+        pathname: pathnameFromStatePath(statePath),
+        statePath: getNormalizedStatePath(statePath),
         contextKey,
         state,
         navigation,
@@ -64,6 +75,56 @@ export function Layout({
       <NavigationContent>{children}</NavigationContent>
     </LayoutContext.Provider>
   );
+}
+
+function getNormalizedStatePath(statePath: string) {
+  const pathname =
+    "/" +
+    (statePath
+      .split("/")
+      .map((value) => decodeURIComponent(value))
+      .filter(Boolean)
+      .join("/") || "");
+
+  return pathname.split("?")![0];
+}
+
+function pathnameFromStatePath(statePath: string) {
+  const pathname =
+    "/" +
+    (statePath
+      .split("/")
+      .map((value) => {
+        const segment = decodeURIComponent(value);
+        if (matchFragmentName(segment) != null || segment === "index") {
+          return null;
+        }
+        return segment;
+      })
+      .filter(Boolean)
+      .join("/") || "");
+
+  const components = pathname.split("?");
+
+  return resolveHref({
+    pathname: components[0],
+    // TODO: This is not efficient, we should generate based on the state instead
+    // of converting to string then back to object
+    query: parseQueryString(components[1] ?? ""),
+  });
+}
+
+function parseQueryString(val: string) {
+  if (!val) {
+    return {};
+  }
+  const query = {};
+  const a = val.split("&");
+  for (let i = 0; i < a.length; i++) {
+    const b = a[i].split("=");
+    query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || "");
+  }
+  return query;
 }
 
 export function useLayoutContext() {
