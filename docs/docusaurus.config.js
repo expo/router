0b5fb05fa17b5575d48042834ec24aac0e9334a8
evanbacon <bacon@expo.io>
2022-10-04 12:23:06 -0500

    add og image

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -49,6 +49,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: "img/og-image.png",
       navbar: {
         title: "Expo Router",
         logo: {
