af94c0f88c22b3f57f26992989ee9969a77ccedb
Evan Bacon <bacon@expo.io>
2022-11-09 12:37:38 -0600

    Update docusaurus.config.js (#125)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -50,6 +50,30 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "MNYP430BS2",
+
+        // Public API key: it is safe to commit it
+        apiKey: "109a300c47881b9e264a48050730d46c",
+
+        indexName: "expo-router",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
+
       announcementBar: {
         id: "beta_notice_1",
         content:
