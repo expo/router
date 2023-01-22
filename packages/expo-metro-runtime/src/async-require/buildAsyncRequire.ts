1b147482c146a57a34f0a32a16ce2e04b9c54e28
evanbacon <bacon@expo.io>
2023-01-22 17:25:35 -0600

    add license

--- packages/expo-metro-runtime/src/async-require/buildAsyncRequire.ts
+++ packages/expo-metro-runtime/src/async-require/buildAsyncRequire.ts
@@ -1,9 +1,12 @@
+/**
+ * Copyright © 2022 650 Industries.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { loadBundleAsync } from "./loadBundle";
 
 type ImportBundleNames = Record<string, string>;
-// type ImportBundleNames = Record<string, string> & {
-//   __proto__: null;
-// };
 
 type MetroRequire = {
   (id: number): any;
