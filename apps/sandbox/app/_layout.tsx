8080fd97478ba45e0623357651a0dcbec888a152
evanbacon <bacon@expo.io>
2023-01-24 13:38:18 -0600

    add og

--- apps/sandbox/app/_layout.tsx
+++ apps/sandbox/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Link, Navigator, Tabs } from "expo-router";
+import { Head, Link, Navigator, Tabs } from "expo-router";
 import { Platform } from "react-native";
 
 import "../theme.css";
@@ -6,13 +6,23 @@ import "../theme.css";
 export default function Layout() {
   // if (Platform.OS !== "web") {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="web-lib" />
-      <Tabs.Screen name="posts" />
-      <Tabs.Screen name="nested/settings" />
-      <Tabs.Screen name="nested/[id]" />
-    </Tabs>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* og image */}
+        <meta property="og:image" content="/og/og-image.jpg" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="720" />
+        <meta property="og:image:type" content="image/jpeg" />
+      </Head>
+      <Tabs>
+        <Tabs.Screen name="index" />
+        <Tabs.Screen name="web-lib" />
+        <Tabs.Screen name="posts" />
+        <Tabs.Screen name="nested/settings" />
+        <Tabs.Screen name="nested/[id]" />
+      </Tabs>
+    </>
   );
   // }
   return (
