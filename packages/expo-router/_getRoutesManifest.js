80e1c210b08ce7e8d18a55ecf01fe567ec9990a7
evanbacon <bacon@expo.io>
2023-02-01 16:15:29 -0600

    Update _getRoutesManifest.js

--- packages/expo-router/_getRoutesManifest.js
+++ packages/expo-router/_getRoutesManifest.js
@@ -3,14 +3,13 @@ import { getRoutes } from "./build/getRoutes";
 
 /** Get the linking manifest from a Node.js process. */
 export function getManifestFromContextModule(contextModule) {
-  const routeTree = getRoutes(contextModule, { preserveApiRoutes: true });
+  const routeTree = getRoutes(contextModule);
   if (!routeTree) {
     return {};
   }
   return getNavigationConfig(routeTree);
 }
 
-// Must be exported or Fast Refresh won't update the context >:[
 export function getManifest() {
   const ctx = require.context(process.env.EXPO_ROUTER_APP_ROOT);
   return getManifestFromContextModule(ctx);
