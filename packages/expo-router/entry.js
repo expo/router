43018cd61a73dc14d20b7353aaa41ad744a95df6
evanbacon <bacon@expo.io>
2023-02-03 21:00:02 -0600

    fix fast refresh

--- packages/expo-router/entry.js
+++ packages/expo-router/entry.js
@@ -1,16 +1,21 @@
+import "@expo/metro-runtime";
+
+import { ExpoRoot } from "expo-router";
 import Head from "expo-router/head";
 
-import ExpoRouterRoot from "./_root";
 import { renderRootComponent } from "./build/renderRootComponent";
 
 // We add this elsewhere for rendering
 const HeadProvider =
   typeof window === "undefined" ? React.Fragment : Head.Provider;
 
-function App() {
+const ctx = require.context(process.env.EXPO_ROUTER_APP_ROOT);
+
+// Must be exported or Fast Refresh won't update the context >:[
+export function App() {
   return (
     <HeadProvider>
-      <ExpoRouterRoot />
+      <ExpoRoot context={ctx} />
     </HeadProvider>
   );
 }
