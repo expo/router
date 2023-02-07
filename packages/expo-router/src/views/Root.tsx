a44e2c3b87e192090005ffd420934f7542fbc375
evanbacon <bacon@expo.io>
2023-02-07 12:40:52 -0600

    adjust safe area provider to work better in static rendering

--- packages/expo-router/src/views/Root.tsx
+++ packages/expo-router/src/views/Root.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
+import { Platform, Dimensions } from "react-native";
 import { ContextNavigator } from "../ContextNavigator";
 import Head from "../head/Head";
 import { RequireContext } from "../types";
@@ -25,19 +28,24 @@ const GestureHandlerRootView = getGestureHandlerRootView();
 const EnsureHelmetProvider =
   typeof window === "undefined" ? React.Fragment : Head.Provider;
 
-const INITIAL_METRICS = {
-  frame: { x: 0, y: 0, width: 0, height: 0 },
-  insets: { top: 0, left: 0, right: 0, bottom: 0 },
-};
+const { width = 0, height = 0 } = Dimensions.get("window");
+
+// To support SSR on web, we need to have empty insets for initial values
+// Otherwise there can be mismatch between SSR and client output
+// We also need to specify empty values to support test environments
+const INITIAL_METRICS =
+  Platform.OS === "web" || initialWindowMetrics == null
+    ? {
+        frame: { x: 0, y: 0, width, height },
+        insets: { top: 0, left: 0, right: 0, bottom: 0 },
+      }
+    : initialWindowMetrics;
 
 export function ExpoRoot({ context }: { context: RequireContext }) {
   return (
     <GestureHandlerRootView>
       <EnsureHelmetProvider>
-        <SafeAreaProvider
-          // SSR support
-          initialMetrics={INITIAL_METRICS}
-        >
+        <SafeAreaProvider initialMetrics={INITIAL_METRICS}>
           <ContextNavigator context={context} />
           {/* Users can override this by adding another StatusBar element anywhere higher in the component tree. */}
           <StatusBar style="auto" />
