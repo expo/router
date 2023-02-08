df55220a361e07eb84ebf24dc931c7c2cf1bbe73
Evan Bacon <bacon@expo.io>
2023-02-07 20:32:45 -0600

    divide root and fork registerRootComponent (#251)
    * use new root format
    
    * wip
    
    * fix initial state on web
    
    * move render to router
    
    * fix fast refresh
    
    * fix build issues

--- packages/expo-router/src/views/Root.tsx
+++ packages/expo-router/src/views/Root.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { ContextNavigator } from "../ContextNavigator";
-import Head from "../head/Head";
 import { RequireContext } from "../types";
 
 function getGestureHandlerRootView() {
@@ -21,10 +20,6 @@ function getGestureHandlerRootView() {
 
 const GestureHandlerRootView = getGestureHandlerRootView();
 
-// We add this elsewhere for rendering
-const EnsureHelmetProvider =
-  typeof window === "undefined" ? React.Fragment : Head.Provider;
-
 const INITIAL_METRICS = {
   frame: { x: 0, y: 0, width: 0, height: 0 },
   insets: { top: 0, left: 0, right: 0, bottom: 0 },
@@ -33,16 +28,14 @@ const INITIAL_METRICS = {
 export function ExpoRoot({ context }: { context: RequireContext }) {
   return (
     <GestureHandlerRootView>
-      <EnsureHelmetProvider>
-        <SafeAreaProvider
-          // SSR support
-          initialMetrics={INITIAL_METRICS}
-        >
-          <ContextNavigator context={context} />
-          {/* Users can override this by adding another StatusBar element anywhere higher in the component tree. */}
-          <StatusBar style="auto" />
-        </SafeAreaProvider>
-      </EnsureHelmetProvider>
+      <SafeAreaProvider
+        // SSR support
+        initialMetrics={INITIAL_METRICS}
+      >
+        <ContextNavigator context={context} />
+        {/* Users can override this by adding another StatusBar element anywhere higher in the component tree. */}
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
