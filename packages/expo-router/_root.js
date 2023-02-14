af7e4391f95819bd0d6f0d79b6d6bbd19f33e8af
evanbacon <bacon@expo.io>
2023-02-14 12:07:32 -0600

    Update _root.js

--- packages/expo-router/_root.js
+++ packages/expo-router/_root.js
@@ -5,7 +5,13 @@ import { ExpoRoot } from "expo-router";
 import { getNavigationConfig } from "./build/getLinkingConfig";
 import { getRoutes } from "./build/getRoutes";
 
-const ctx = require.context(process.env.EXPO_ROUTER_APP_ROOT);
+// const ctx = require.context(process.env.EXPO_ROUTER_APP_ROOT);
+const ctx = require.context(
+  process.env.EXPO_ROUTER_APP_ROOT,
+  true,
+  /.*/,
+  "lazy"
+);
 
 // Must be exported or Fast Refresh won't update the context >:[
 export default function ExpoRouterRoot() {
