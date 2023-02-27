1016a38746efd02b7789588291b2fe12ccdfcedf
Evan Bacon <bacon@expo.io>
2023-02-27 15:04:49 -0600

    Bump to metro@0.75.0 (#327)
    * update expo-metro-runtime
    
    * Bump to metro@0.75.0
    
    * fix tests

---
+++ packages/expo-metro-runtime/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+## Unpublished
+
+### 🛠 Breaking changes
+
+### 🎉 New features
+
+- Update `asyncRequireModulePath` to support `metro@0.75.0`
+
+### 🐛 Bug fixes
+
+### 💡 Others
--- packages/expo-metro-runtime/src/async-require/__tests__/buildAsyncRequire.test.ts
+++ packages/expo-metro-runtime/src/async-require/__tests__/buildAsyncRequire.test.ts
@@ -16,28 +16,34 @@ function getMockRequire() {
   return mockRequire;
 }
 
+const originalEnv = process.env.NODE_ENV;
+beforeEach(() => {
+  process.env.NODE_ENV = "development";
+});
+
+afterAll(() => {
+  process.env.NODE_ENV = originalEnv;
+});
+
 it(`builds required object`, async () => {
   const _require = getMockRequire();
   const asyncRequire = buildAsyncRequire(_require);
 
   expect(asyncRequire).toBeInstanceOf(Function);
-  expect(asyncRequire.addImportBundleNames).toBeInstanceOf(Function);
   expect(asyncRequire.prefetch).toBeInstanceOf(Function);
   expect(asyncRequire.resource).toBeInstanceOf(Function);
 });
 
-it(`loads the module with \`require.importAll\` if the moduleID was not registered before invocation`, async () => {
+it(`loads the module with \`loadBundleAsync\` if the module has not been loaded already`, async () => {
   const _require = getMockRequire();
   const asyncRequire = buildAsyncRequire(_require);
 
-  const myModule = asyncRequire(650);
-  expect(myModule).toBeUndefined();
+  const myModule = asyncRequire(650, "", { "650": "SixFiveZero" });
+  expect(myModule).toEqual(expect.any(Promise));
 
-  // Didn't call the fetch/async method
-  expect(loadBundleAsync).not.toBeCalled();
-
-  // Did call whatever this is
-  expect(_require.importAll).toBeCalledWith(650);
+  // Did attempt to fetch the bundle
+  expect(loadBundleAsync).toBeCalledWith("SixFiveZero");
+  expect(_require.importAll).not.toBeCalled();
 });
 
 it(`fetches and returns an async module`, async () => {
@@ -47,19 +53,42 @@ it(`fetches and returns an async module`, async () => {
 
   const asyncRequire = buildAsyncRequire(_require);
 
-  asyncRequire.addImportBundleNames({
-    "2": "Two",
-  });
-
   expect(asyncRequire).toBeInstanceOf(Function);
 
-  const myModule = await asyncRequire(2);
+  const myModule = await asyncRequire(2, "", {
+    "2": "Two",
+  });
 
   // Fetch and load the bundle into memory.
   expect(loadBundleAsync).toBeCalledWith("Two");
 
   // Ensure the module was required using Metro after the bundle was loaded.
   expect(_require).toBeCalledWith(2);
+
+  // Ensure the module was returned.
+  expect(myModule).toEqual({ foo: "bar" });
+});
+
+it(`disables async requires in production`, async () => {
+  process.env.NODE_ENV = "production";
+  const _require = getMockRequire();
+
+  asMock(_require.importAll).mockReturnValueOnce({ foo: "bar" });
+
+  const asyncRequire = buildAsyncRequire(_require);
+
+  expect(asyncRequire).toBeInstanceOf(Function);
+
+  const myModule = await asyncRequire(2, "", {
+    "2": "Two",
+  });
+
+  // Fetch and load the bundle into memory.
+  expect(loadBundleAsync).not.toBeCalled();
+
+  // Ensure the module was required using Metro after the bundle was loaded.
+  expect(_require.importAll).toBeCalledWith(2);
+  expect(_require).not.toBeCalled();
 
   // Ensure the module was returned.
   expect(myModule).toEqual({ foo: "bar" });
