44802d46b4e64d3c4fcaf6615f49c8cc941b4d35
Evan Bacon <bacon@expo.io>
2023-02-27 17:40:35 -0600

    improve logs on web (#329)
    * upgrade to SDK 48
    
    * Update package.json
    
    * update types
    
    * update jest
    
    * fix logs
    
    * fix logs

--- packages/expo-metro-runtime/CHANGELOG.md
+++ packages/expo-metro-runtime/CHANGELOG.md
@@ -6,6 +6,7 @@
 
 ### 🎉 New features
 
+- Log more clearly that logs go to the browser console.
 - Update `asyncRequireModulePath` to support `metro@0.75.0`
 
 ### 🐛 Bug fixes
--- packages/expo-metro-runtime/src/setupHMR.ts
+++ packages/expo-metro-runtime/src/setupHMR.ts
@@ -4,7 +4,30 @@ import HMRClient from "./HMRClient";
 
 // Sets up developer tools for React Native web.
 if (!Platform.isTesting) {
-  HMRClient.log("log", [`JavaScript logs will appear in your browser console`]);
+  // We assume full control over the console and send JavaScript logs to Metro.
+  // [
+  //   'trace',
+  //   'info',
+  //   'warn',
+  //   'error',
+  //   'log',
+  //   'group',
+  //   'groupCollapsed',
+  //   'groupEnd',
+  //   'debug',
+  // ].forEach(level => {
+  //   const originalFunction = console[level];
+  //   console[level] = function (...args: readonly any[]) {
+  //     HMRClient.log(
+  //       // @ts-expect-error
+  //       level, args);
+  //     originalFunction.apply(console, args);
+  //   };
+  // });
+
+  HMRClient.log("log", [
+    `[${Platform.OS}] Logs will appear in the browser console`,
+  ]);
 }
 
 // This is called native on native platforms
