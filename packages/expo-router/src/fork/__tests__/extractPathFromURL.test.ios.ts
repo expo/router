57760787d748198c4ce1a6321a8d719ac2c06c08
evanbacon <bacon@expo.io>
2023-03-21 15:58:40 -0500

    fix #362

--- packages/expo-router/src/fork/__tests__/extractPathFromURL.test.ios.ts
+++ packages/expo-router/src/fork/__tests__/extractPathFromURL.test.ios.ts
@@ -9,38 +9,71 @@ describe(extractExpoPathFromURL, () => {
     Constants.executionEnvironment = originalExecutionEnv;
   });
 
-  test.each<string>([
-    "exp://127.0.0.1:19000/",
-    "exp://127.0.0.1:19000/--/test/path?query=param",
-    "exp://127.0.0.1:19000?query=param",
-    "exp://exp.host/@test/test/--/test/path?query=param",
-    "exp://exp.host/@test/test/--/test/path",
-    "exp://exp.host/@test/test/--/test/path/--/foobar",
-    "https://example.com/test/path?query=param",
-    "https://example.com/test/path",
-    "https://example.com:8000/test/path",
-    "https://example.com:8000/test/path+with+plus",
-    "https://example.com/test/path?query=do+not+escape",
-    "https://example.com/test/path?missingQueryValue=",
-    "custom:///?shouldBeEscaped=x%252By%2540xxx.com",
-    "custom:///test/path?foo=bar",
-    "custom:///",
-    "custom://",
-    "custom://?hello=bar",
-    "invalid",
-  ])(`parses %p`, (url) => {
-    Constants.executionEnvironment = ExecutionEnvironment.StoreClient;
+  for (const [name, exenv] of [
+    ["Expo Go", ExecutionEnvironment.StoreClient],
+    ["Bare", ExecutionEnvironment.Bare],
+  ] as const) {
+    describe(name, () => {
+      test.each<string>([
+        "exp://127.0.0.1:19000/",
+        "exp://127.0.0.1:19000/--/test/path?query=param",
+        "exp://127.0.0.1:19000/--/test/path?shouldBeEscaped=x%252By%2540xxx.com",
+        "exp://127.0.0.1:19000/x?y=x%252By%2540xxx.com",
+        "exp://127.0.0.1:19000?query=param",
+        "exp://exp.host/@test/test/--/test/path?query=param",
+        "exp://exp.host/@test/test/--/test/path",
+        "exp://exp.host/@test/test/--/test/path/--/foobar",
+        "https://example.com/test/path?query=param",
+        "https://example.com/test/path",
+        "https://example.com:8000/test/path",
+        "https://example.com:8000/test/path+with+plus",
+        "https://example.com/test/path?query=do+not+escape",
+        "https://example.com/test/path?missingQueryValue=",
+        "custom:///?shouldBeEscaped=x%252By%2540xxx.com",
+        "custom:///test/path?foo=bar",
+        "custom:///",
+        "custom://",
+        "custom://?hello=bar",
+        "invalid",
+      ])(`parses %p`, (url) => {
+        Constants.executionEnvironment = exenv;
 
-    const res = extractExpoPathFromURL(url);
-    expect(res).toMatchSnapshot();
-    // Ensure the Expo Go handling never breaks
-    expect(res).not.toMatch(/^--\//);
+        const res = extractExpoPathFromURL(url);
+        expect(res).toMatchSnapshot();
+
+        if (exenv === ExecutionEnvironment.StoreClient) {
+          // Ensure the Expo Go handling never breaks
+          expect(res).not.toMatch(/^--\//);
+        }
+      });
+    });
+  }
+  it(`decodes query params`, () => {
+    Constants.executionEnvironment = ExecutionEnvironment.Bare;
+    expect(extractExpoPathFromURL(`custom:///?x=x%252By%2540xxx.com`)).toEqual(
+      "?x=x+y@xxx.com"
+    );
+
+    Constants.executionEnvironment = ExecutionEnvironment.StoreClient;
+    expect(extractExpoPathFromURL(`custom:///?x=x%252By%2540xxx.com`)).toEqual(
+      "?x=x+y@xxx.com"
+    );
+    expect(
+      extractExpoPathFromURL(
+        `exp://127.0.0.1:19000/--/test/path?x=x%252By%2540xxx.com`
+      )
+    ).toEqual("test/path?x=x+y@xxx.com");
+    expect(extractExpoPathFromURL(`exp://x?y=x%252By%2540xxx.com`)).toEqual(
+      "?y=x+y@xxx.com"
+    );
   });
 
   it(`only handles Expo Go URLs in Expo Go`, () => {
     Constants.executionEnvironment = ExecutionEnvironment.Bare;
 
     const res = extractExpoPathFromURL("exp://127.0.0.1:19000/--/test");
-    expect(res).toEqual("--/test");
+    // This should look mostly broken, but it's the best we can do
+    // when someone uses this format outside of Expo Go.
+    expect(res).toEqual("127.0.0.1:19000/--/test");
   });
 });
