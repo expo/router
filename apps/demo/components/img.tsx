d6f3850fca31af83f6bea2a8833fe3131c903af2
evanbacon <bacon@expo.io>
2023-03-25 15:16:44 -0500

    wip -- idk

--- apps/demo/components/img.tsx
+++ apps/demo/components/img.tsx
@@ -1,10 +1,14 @@
 import { Image, ImageProps } from "expo-image";
 
 // expo-image doesn't support accessibility props on web :[
-export default function BetterImage(props: ImageProps & { alt?: string }) {
+export default function BetterImage(
+  props: ImageProps & { alt?: string; width?: number; height?: number }
+) {
+  const { width, height, style, ...rest } = props;
   return (
     <Image
-      {...props}
+      {...rest}
+      style={[style, { width, height }]}
       accessibilityLabel={props.alt ?? props.accessibilityLabel}
     />
   );
