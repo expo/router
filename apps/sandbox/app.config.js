ca238d2282384c15a96de588cc077b3afad60b85
evanbacon <bacon@expo.io>
2023-03-30 11:52:50 -0500

    wip

---
+++ apps/sandbox/app.config.js
@@ -0,0 +1,26 @@
+process.env.EXPO_TUNNEL_SUBDOMAIN = "bacon-router-sandbox";
+
+/** @type {import('expo/config').ExpoConfig} */
+module.exports = {
+  name: "Router Sandbox",
+  slug: "expo-router-sandbox",
+  scheme: "sandbox",
+  splash: {
+    image: "./splash.png",
+    backgroundColor: "#1c2026",
+  },
+  web: {
+    bundler: "metro",
+  },
+  ios: {
+    bundleIdentifier: "app.expo.router.sandbox",
+    infoPlist: {
+      CoreSpotlightContinuation: true,
+      associatedDomains: [
+        "applinks:bacon-router-sandbox.ngrok.io?mode=developer",
+        "webcredentials:bacon-router-sandbox.ngrok.io?mode=developer",
+        "activitycontinuation:bacon-router-sandbox.ngrok.io?mode=developer",
+      ],
+    },
+  },
+};
