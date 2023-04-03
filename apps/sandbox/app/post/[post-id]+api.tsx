ca0efc5776b3fb7dcb2e338696b1546b8e550eb5
evanbacon <bacon@expo.io>
2023-04-03 12:49:25 -0500

    Update [post-id]+api.tsx

--- apps/sandbox/app/post/[post-id]+api.tsx
+++ apps/sandbox/app/post/[post-id]+api.tsx
@@ -2,11 +2,8 @@ import { ExpoRequest, ExpoResponse } from "expo-router/server";
 
 const SUPER_E2E_TEST_SECRET_VALUE = "SUPER_E2E_TEST_SECRET_VALUE";
 
-/** @type import('expo-router/server').RequestHandler */
+/** @type {import('expo-router/server').RequestHandler} */
 export async function GET(req: ExpoRequest) {
   console.log(">", req.expoUrl, req.expoUrl.searchParams.get("post-id"));
   return ExpoResponse.json({ hey: "world" });
-  // return Response.json({
-  //   message: "Hello World!",
-  // });
 }
