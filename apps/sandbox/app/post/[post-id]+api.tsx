231c2edd8bed84781b0178abfd99f3d658e6fcb7
evanbacon <bacon@expo.io>
2023-04-07 13:02:33 -0500

    wip

--- apps/sandbox/app/post/[post-id]+api.tsx
+++ apps/sandbox/app/post/[post-id]+api.tsx
@@ -1,12 +1,7 @@
 // import { ExpoRequest, ExpoResponse } from "expo-router/server";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import fs from "fs";
+import path from "path";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-const ExpoRequest =
-  global.ExpoRequest as typeof import("expo-router/server").ExpoRequest;
 const ExpoResponse =
   global.ExpoResponse as typeof import("expo-router/server").ExpoResponse;
 
@@ -16,11 +11,19 @@ const SUPER_E2E_TEST_SECRET_VALUE = "SUPER_E2E_TEST_SECRET_VALUE";
 export async function GET(
   req: typeof import("expo-router/server").ExpoRequest
 ) {
+  const pkg = JSON.parse(
+    await fs.promises.readFile(
+      path.join(__dirname, "../..", "package.json"),
+      "utf8"
+    )
+  );
+
   console.log(
     ">",
+    pkg,
     { __dirname, __filename },
     req.expoUrl,
     req.expoUrl.searchParams.get("post-id")
   );
-  return ExpoResponse.json({ hey: "world" });
+  return ExpoResponse.json({ hey: "world", pkg, SUPER_E2E_TEST_SECRET_VALUE });
 }
