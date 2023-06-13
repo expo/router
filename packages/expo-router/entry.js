f9f15c178ec23afc6d64b722804d2110f5214184
evanbacon <bacon@expo.io>
2023-06-13 18:42:06 -0500

    fix HMR issues on web

--- packages/expo-router/entry.js
+++ packages/expo-router/entry.js
@@ -1,19 +1,11 @@
 // `@expo/metro-runtime` MUST be the first import to ensure Fast Refresh works
 // on web.
 import "@expo/metro-runtime";
-import { ExpoRoot } from "expo-router";
-import Head from "expo-router/head";
+
+// This file should only import and register the root. No components or exports
+// should be added here.
 import { renderRootComponent } from "expo-router/src/renderRootComponent";
 
-import { ctx } from "./_ctx";
-
-// Must be exported or Fast Refresh won't update the context
-export function App() {
-  return (
-    <Head.Provider>
-      <ExpoRoot context={ctx} />
-    </Head.Provider>
-  );
-}
+import { App } from "./_app";
 
 renderRootComponent(App);
