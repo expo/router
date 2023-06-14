b12818a924f412cd4d04ca62010bba88a9f10151
evanbacon <bacon@expo.io>
2023-06-14 14:12:32 -0500

    Update createEntryFile.test.node.ts

--- packages/expo-router/src/onboard/__tests__/createEntryFile.test.node.ts
+++ packages/expo-router/src/onboard/__tests__/createEntryFile.test.node.ts
@@ -9,6 +9,6 @@ it(`returns correct absolute path for standard projects`, () => {
   expect(getAbsolutePath()).toEqual("/path/to/expo/project/app/index.js");
 });
 it(`returns correct absolute path for standard projects with src dir`, () => {
-  process.env.EXPO_PROJECT_ROOT = "/path/to/expo/project/src/app";
+  process.env.EXPO_ROUTER_ABS_APP_ROOT = "/path/to/expo/project/src/app";
   expect(getAbsolutePath()).toEqual("/path/to/expo/project/src/app/index.js");
 });
