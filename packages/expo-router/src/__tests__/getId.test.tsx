de35ed683c0417f6c0c352dee2d306c3ab048f52
Mark Lawlor <mwlawlor@gmail.com>
2023-06-20 11:23:36 +1000

    fit linter errors

--- packages/expo-router/src/__tests__/getId.test.tsx
+++ packages/expo-router/src/__tests__/getId.test.tsx
@@ -1,10 +1,4 @@
-import React from "react";
-import { Text } from "react-native";
-
-import { Link, router } from "../exports";
 import { generateDynamic } from "../getRoutes";
-import { useLocalSearchParams } from "../hooks";
-import { act, renderRouter, screen } from "../testing-library";
 import { createGetIdForRoute } from "../useScreens";
 
 function createMockRoute(routeName: string) {
