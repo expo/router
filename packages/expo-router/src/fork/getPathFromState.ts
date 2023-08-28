78e0c29e57206a38a210b799a34c73b2de21efc1
Evan Bacon <bacon@expo.io>
2023-08-28 18:10:03 -0500

    Update getPathFromState.ts (#862)

--- packages/expo-router/src/fork/getPathFromState.ts
+++ packages/expo-router/src/fork/getPathFromState.ts
@@ -367,7 +367,7 @@ function getPathFromResolvedState(
   while (current) {
     path += "/";
 
-    const route = current.routes[current.index ?? 0] as CustomRoute;
+    const route = { ...(current.routes[current.index ?? 0] as CustomRoute) };
     // NOTE(EvanBacon): Fill in current route using state that was passed as params.
     // if (isInvalidParams(route.params)) {
     if (!route.state && isInvalidParams(route.params)) {
