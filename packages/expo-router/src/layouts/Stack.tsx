d8eafbb66fe24c08572e355b794aaefcfec1ca8c
muneebahmedayub <muneebahmedayub01@gmail.com>
2023-08-28 18:43:06 +0500

    add-listener-types

--- packages/expo-router/src/layouts/Stack.tsx
+++ packages/expo-router/src/layouts/Stack.tsx
@@ -1,7 +1,9 @@
 import {
   createNativeStackNavigator,
+  NativeStackNavigationEventMap,
   NativeStackNavigationOptions,
 } from "@react-navigation/native-stack";
+import { ParamListBase, StackNavigationState } from "@react-navigation/native";
 
 import { withLayoutContext } from "./withLayoutContext";
 
@@ -9,7 +11,9 @@ const NativeStackNavigator = createNativeStackNavigator().Navigator;
 
 export const Stack = withLayoutContext<
   NativeStackNavigationOptions,
-  typeof NativeStackNavigator
+  typeof NativeStackNavigator,
+  StackNavigationState<ParamListBase>,
+  NativeStackNavigationEventMap
 >(NativeStackNavigator);
 
 export default Stack;
