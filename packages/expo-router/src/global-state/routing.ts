1ed7f01c98224e31b049f425a56f9cec0d57396d
Mark Lawlor <mwlawlor@gmail.com>
2023-11-07 11:26:21 +1000

    fix relative hrefs inside groups (#900)
    backport of https://github.com/expo/expo/pull/25111

--- packages/expo-router/src/global-state/routing.ts
+++ packages/expo-router/src/global-state/routing.ts
@@ -78,16 +78,28 @@ export function linkTo(this: RouterStore, href: string, event?: string) {
   const rootState = navigationRef.getRootState();
 
   if (href.startsWith(".")) {
-    let base =
-      this.linking.getPathFromState?.(rootState, {
-        screens: [],
-        preserveGroups: true,
-      }) ?? "";
+    const base =
+      this.routeInfo?.segments
+        ?.map((segment) => {
+          if (!segment.startsWith("[")) return segment;
 
-    if (base && !base.endsWith("/")) {
-      base += "/..";
-    }
-    href = resolve(base, href);
+          if (segment.startsWith("[...")) {
+            segment = segment.slice(4, -1);
+            const params = this.routeInfo?.params?.[segment];
+            if (Array.isArray(params)) {
+              return params.join("/");
+            } else {
+              return params?.split(",")?.join("/") ?? "";
+            }
+          } else {
+            segment = segment.slice(1, -1);
+            return this.routeInfo?.params?.[segment];
+          }
+        })
+        .filter(Boolean)
+        .join("/") ?? "/";
+
+    href = resolve(base + "/..", href);
   }
 
   const state = this.linking.getStateFromPath!(href, this.linking.config);
